56d6c29f7279142ba30f704487050df82e0614a1
luomingshun <13545033807@163.com>
2018-10-12 00:30:56 +0800

    yi

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,38 +1,56 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length"
-           class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends"
-               :key="item.id">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl"
-                   alt="">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" :key="item.id" class="item">
-            <div class="icon">
-              <img :src="item.imgurl" alt="" width="60" height="60">
+    <scroll ref="scroll"
+            class="recommend-content"
+            :data="discList">
+      <div>
+        <div v-if="recommends.length"
+             class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends"
+                 :key="item.id">
+              <a :href="item.linkUrl">
+                <img @load="loadImage"
+                     :src="item.picUrl"
+                     alt="">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList"
+                :key="item.id"
+                class="item">
+              <div class="icon">
+                <img v-lazy="item.imgurl"
+                     alt=""
+                     width="60"
+                     height="60">
+              </div>
+              <div class="text">
+                <h2 class="name"
+                    v-html="item.creator.name"></h2>
+                <p class="desc"
+                   v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading>
+        </loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
+import Loading from 'base/loading/loading'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 export default {
@@ -44,6 +62,9 @@ export default {
   },
   created() {
     this._getRecommend()
+    // setTimeout(() => {
+    //   this._getDIscList()
+    // }, 3000)
     this._getDIscList()
   },
   methods: {
@@ -62,10 +83,18 @@ export default {
           this.discList = res.data.list
         }
       })
+    },
+    loadImage() {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll,
+    Loading
   }
 }
 </script>

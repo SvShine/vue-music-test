1fe7f4108bab770ec92ae3ca1a1bcd72832cb660
luomingshun <13545033807@163.com>
2019-01-17 23:09:16 +0800

    singerDetail

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
-
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
-
 Vue.use(VueLazyLoad, {
   loading: require('common/image/default.png')
 })
@@ -19,5 +18,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h => h(App),
+  store,
   router
 })
